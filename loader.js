2cccaf0eadd90d4e9a8140a216bb0ca2184e0ccb
Sundredshadow <54553082+Sundredshadow@users.noreply.github.com>
2020-10-27 19:08:11 -0400

    Delete loader.js

--- loader.js
+++
@@ -1,8 +0,0 @@
-import {init}  from "./main.js";
-window.onload = ()=>{
-	console.log("window.onload called");
-	// 1 - do preload here - load fonts, images, additional sounds, etc...
-	
-	// 2 - start up app
-	init();
-}
